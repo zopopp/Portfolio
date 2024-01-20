ca17aae84610b8d45233c4bff4b05e90af5795d2
IMD Pantheon <aneuner@umd.edu>
2024-01-19 23:08:30 -0500

    ADDED descriptions and more things

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function displayMainImage(imageSrc, text) {
+function displayMainImage(imageSrc, text, link) {
     const mainDisplay = document.getElementById("main-display");
     const mainDisplayText = document.getElementById("main-display-text");
     mainDisplay.src = imageSrc;
-    mainDisplayText.innerText = text;
+    mainDisplayText.innerText = text + link;
 }
 
 /*function displayWords(paragraph) {
